98aad4d74f3fb5683dc4ee1a273f3f11f54cc78e
rdmadeira <rdmadeira2@gmail.com>
2022-08-07 13:25:35 -0300

    resolved error on path index.js

--- scripts/user/index.js
+++ scripts/user/index.js
@@ -2,10 +2,10 @@ const loginBtn = document.getElementById('login-btn');
 const signUpBtn = document.getElementById('signup-btn');
 
 function appearLogin() {
-    location.href = '.public/user/login.html'
+    location.href = './public/user/login.html'
 }
 function appearRegister() {
-    location.href = '.public/user/register.html';
+    location.href = './public/user/register.html';
 }
 
 loginBtn.addEventListener('click', ()=> appearLogin());
